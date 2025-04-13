9087cd6d6902c9ae829fdffdcc136956183c00cd
Anjaneya Reddy (IGIDR, Mumbai) <areddy.ragini@gmail.com>
2025-04-13 11:25:49 +0530

    Create vectorstore.py

---
+++ vectorstore.py
@@ -0,0 +1,12 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+
+def create_vector_store(docs):
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local("faiss_index")
+    return vectorstore
+
+def load_vector_store():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return FAISS.load_local("faiss_index", embeddings)
