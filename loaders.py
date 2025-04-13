b787ef6ec4ce8acf6ab7b0f5bb327e15da8b2bfe
Anjaneya Reddy (IGIDR, Mumbai) <areddy.ragini@gmail.com>
2025-04-13 11:46:51 +0530

    Update loaders.py

--- loaders.py
+++ loaders.py
@@ -6,17 +6,17 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 def load_documents():
     docs = []
 
-    if os.path.exists("data/example.pdf"):
-        pdf_loader = PyPDFLoader("data/example.pdf")
+    if os.path.exists("data/igidr_library_details.pdf"):
+        pdf_loader = PyPDFLoader("data/igidr_library_details.pdf")
         docs += pdf_loader.load()
     else:
-        print("⚠️ PDF file not found: data/example.pdf")
+        print("⚠️ PDF file not found: data/igidr_library_details.pdf")
 
-    if os.path.exists("data/example.html"):
-        html_loader = BSHTMLLoader("data/example.html")
+    if os.path.exists("data/library-2/"):
+        html_loader = BSHTMLLoader("data/li.html")
         docs += html_loader.load()
     else:
-        print("⚠️ HTML file not found: data/example.html")
+        print("⚠️ HTML file not found: data/li.html")
 
     if not docs:
         raise ValueError("No valid documents found in the data/ directory.")
